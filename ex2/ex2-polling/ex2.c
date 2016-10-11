18cc56cea61aa0775842d1ed18e72f5911e91462
unknown <haakonjonsson@gmail.com>
2016-10-11 04:56:22 +0200

    Fixed spacing

--- ex2/ex2-polling/ex2.c
+++ ex2/ex2-polling/ex2.c
@@ -4,7 +4,7 @@
 #include "efm32gg.h"
 
 /* The period between sound samples, in clock cycles */
-#define   SAMPLE_PERIOD   292 // 14 MHz / 292 = 48 KHz
+#define	SAMPLE_PERIOD	292 // 14 MHz / 292 = 48 KHz
 
 typedef uint32_t uint;
 typedef uint32_t fp;
@@ -17,39 +17,50 @@ void setupTimer(uint32_t period);
 void setupDAC();
 
 /*
-   This implementation uses a fixed point number format.
-   It stores positive numbers in a uint32_t, with 16 bit before and after the radix point.
-   A whole positive number can be converted to the fixed point format,
-   by bit shifting left, or multiply by 2^16.
-   And back, by bit shifting right, or divide by 2^16.
-   The format does not store negative numbers.
-   This is done to avoid having to use slow floating point numbers.
-   It is specified when a function takes, or returns, this format.
+	This implementation uses a fixed point number format.
+	It stores positive numbers in a uint32_t, with 16 bit before and after the radix point.
+	A whole positive number can be converted to the fixed point format,
+	by bit shifting left, or multiply by 2^16.
+	And back, by bit shifting right, or divide by 2^16.
+	The format does not store negative numbers.
+	This is done to avoid having to use slow floating point numbers.
+	It is specified when a function takes, or returns, this format.
 */
 
 /*
-   Declaration of fixed point multiplication, implemented in fixed_point_mul.s
-   Used to multiply two fixed point numbers.
-   Also returns a fixed point number.
-   For multiplication between a fixed point and a normal number,
-   normal multiplication can be used.
+	Declaration of fixed point multiplication, implemented in fixed_point_mul.s
+	Used to multiply two fixed point numbers.
+	Also returns a fixed point number.
+	For multiplication between a fixed point and a normal number,
+	normal multiplication can be used.
 */
 extern fp fixed_point_mul(fp a, fp b);
 
 /*
-   Returns frequency of note in specified octave, tuned to A4 = 440Hz.
-   Takes normal numbers, and return fixed point number.
-   Note should be a value from 0-11 with the following value:
-   C = 0, c# = 1, D = 2, D# = 3, E = 4, F = 5, F# = 6, G = 7, G# = 8, A = 9, A# = 10, B = 11
+	Returns frequency of note in specified octave, tuned to A4 = 440Hz.
+	Takes normal numbers, and return fixed point number.
+	Note should be a value from 0-11 with the following value:
+	C = 0, c# = 1, D = 2, D# = 3, E = 4, F = 5, F# = 6, G = 7, G# = 8, A = 9, A# = 10, B = 11
 */
 fp getFrequency(uint note, uint octave);
 
 /*
-   Function called when new data should be pushed to the DAC.
-   Function is called at the sample rate, 48 KHz.
-   time is the time sinze the start in sample rate tick.
+	Function called when new data should be pushed to the DAC.
+	Function is called at the sample rate, 48 KHz.
+	time is the time sinze the start in sample rate ticks.
 */
 void pushDataToDAC(uint time);
+
+/*
+	Functions for generating waveforms.
+	They take a frequency in fixed point format,
+	and a time in sample rate ticks,
+	and return a value between 0 and 1,
+	in fixed point format.
+*/
+fp sawWave(fp frequency, uint time);
+fp squareWave(fp frequency, uint time);
+fp triangleWave(fp frequency, uint time);
 
 int main(void)
 {
@@ -58,8 +69,6 @@ int main(void)
 	setupDAC();
 	setupTimer(SAMPLE_PERIOD);
 	
-	
-	
 	// Polling loop
 	uint count = 0; 
 	uint lastTimerValue = 0;
@@ -67,7 +76,7 @@ int main(void)
 		uint timerValue = *TIMER1_CNT;
 		
 		// Check if new data should be pushed to the DAC.
-		if(timerValue <= 150 && lastTimerValue > 150){
+		if (timerValue <= 150 && lastTimerValue > 150) {
 			pushDataToDAC(count);
 			
 			if (count == 4294967295) count = 0;
@@ -79,13 +88,43 @@ int main(void)
 	return 0;
 }
 
+
+
 void pushDataToDAC(uint count)
 {
 	*DAC0_CH0DATA = count%109;
 	*DAC0_CH1DATA = count%109;
 }
 
-fp getFrequency(uint note, uint octave);
+
+
+fp sawWave(fp frequency, uint time)
+{
+	uint period = (sample_rate << 16) / frequency; // Find wave period.
+	fp value = (time % period) << 16; // Value is in range 0 to period.
+	value /= period; // Value is in range 0 to 1.
+	return value;
+}
+
+fp squareWave(fp frequency, uint time)
+{
+	uint period = (sample_rate << 16) / frequency; // Find wave period.
+	uint time_point = (time % period); // Find time point in period.
+	if (time_point < period / 2) return 0;
+	else return 1<<16;
+}
+
+fp triangleWave(fp frequency, uint time);
+{
+	uint period = (sample_rate << 16) / frequency; // Find wave period.
+	uint time_point = (time % period); // Find time point in period.
+	if (time_point < period / 2) return time_point<<17 / period;
+	else return (period-time_point)<<17 / period;
+}
+
+
+
+fp getFrequency(uint note, uint octave)
 {
 	// 440 * 2^((octave*12 + note - 57)/12) =
 	// 440 / 2^(57/12) * 2^octave * 2^(note/12)
