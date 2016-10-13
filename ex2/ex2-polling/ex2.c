2aa3ac572047b862581098edbde303d98ee98dc8
Haakon Jonsson <haakonjonsson@gmail.com>
2016-10-13 13:31:49 +0200

    Moved struct to header

--- ex2/ex2-polling/ex2.c
+++ ex2/ex2-polling/ex2.c
@@ -3,19 +3,10 @@
 
 #include "efm32gg.h"
 
+#include "ex2.h"
+
 /* The period between sound samples, in clock cycles */
 #define	SAMPLE_PERIOD	292 // 14 MHz / 292 = 48 KHz
-
-typedef uint32_t uint;
-typedef uint32_t fp;
-
-typedef struct Song
-{
-	uint* song;
-	uint length;
-	uint tempo;
-	bool looping;
-} Song_t;
 
 static const uint sample_rate = 14000000 / SAMPLE_PERIOD;
 static const uint volume = 128;
