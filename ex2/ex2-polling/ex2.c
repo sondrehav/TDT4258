72196469bd8fdabc3b75e381ccc298164bc30a3e
Haakon Jonsson <haakonjonsson@gmail.com>
2016-10-14 12:36:03 +0200

    fix

--- ex2/ex2-polling/ex2.c
+++ ex2/ex2-polling/ex2.c
@@ -16,46 +16,11 @@ void setupDAC();
 
 
 
+extern void audioSetup();
+extern audio_t audio;
+extern uint time;
+
 void selectSong();
-
-/* Sound data and variables. */
-uint songData[] = {
-	48, 50, 52, 53, 55, 55, 55, 55, 57, 57, 57, 57, 
-	55, 55, 55, 55, 53, 53, 53, 53, 52, 52, 52, 52, 
-	50, 50, 50, 50, 48, 48, 48, 48
-};
-
-uint songData2[] = {
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 50, 55, 62, 65, 55, 62, 65, 
-	48, 50, 55, 62, 65, 55, 62, 65, 
-	47, 50, 55, 61, 65, 55, 61, 65, 
-	47, 50, 55, 61, 65, 55, 61, 65, 
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 52, 55, 60, 64, 55, 60, 64
-};
-
-uint song1up[] = {
-   64, 67, 76, 72, 74, 79
-};
-
-uint songData4[] = {
-	100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0
-};
-
-
-
-song_t songs[4];
-soundPlayer_t soundPlayers[4];
-audio_t audio = {soundPlayers, 4};
-
-song_t song1 = {songData, 32, 240, 0};
-song_t song2 = {songData2, 64, 480, 0};
-song_t song3 = {song1up, 6, 360, 0};
-song_t song4 = {songData4, 11, 120, 0};
-
-uint time;
 
 int main(void)
 {
@@ -64,23 +29,7 @@ int main(void)
 	setupDAC();
 	setupTimer(SAMPLE_PERIOD);
 	
-	/* Audio setup. */
-	songs[0] = song1;
-	songs[1] = song2;
-	songs[2] = song3;
-	songs[3] = song4;
-	
-	initSoundPlayer(soundPlayers, songs, Saw, 4);
-	initSoundPlayer(soundPlayers+1, songs+1, Saw, 4);
-	initSoundPlayer(soundPlayers+2, songs+2, Square, 4);
-	initSoundPlayer(soundPlayers+3, songs+3, Saw, 4);
-	
-	soundPlayers[0].state = Paused;
-	soundPlayers[1].state = Paused;
-	soundPlayers[2].state = Paused;
-	soundPlayers[3].state = Paused;
-	
-	time = 0;
+	audioSetup();
 	
 	// Polling loop
 	uint lastTimerValue = 0;
--- ex2/ex2-polling/songs.c
+++ ex2/ex2-polling/songs.c
@@ -1,5 +1,68 @@
-uint song[] = {
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+#include "ex2.h"
+#include "sound_generator.h"
+#include "sound_player.h"
+
+/* Sound data and variables. */
+
+uint songStartup[] = {
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64
+};
+
+uint song1up[] = {
+   64, 67, 76, 72, 74, 79
+};
+
+uint songData1[] = {
 	48, 50, 52, 53, 55, 55, 55, 55, 57, 57, 57, 57, 
 	55, 55, 55, 55, 53, 53, 53, 53, 52, 52, 52, 52, 
 	50, 50, 50, 50, 48, 48, 48, 48
 };
+
+uint songData2[] = {
+	100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0
+};
+
+song_t song1 = {songStartup, 64, 480, 0};
+song_t song2 = {song1up, 6, 360, 0};
+song_t song3 = {songData1, 32, 240, 0};
+song_t song4 = {songData2, 11, 120, 0};
+
+
+
+song_t songs[4];
+soundPlayer_t soundPlayers[4];
+audio_t audio = {soundPlayers, 4};
+uint time;
+
+
+
+void audioSetup() {
+	songs[0] = song1;
+	songs[1] = song2;
+	songs[2] = song3;
+	songs[3] = song4;
+	
+	initSoundPlayer(soundPlayers, songs, Saw, 4);
+	initSoundPlayer(soundPlayers+1, songs+1, Saw, 4);
+	initSoundPlayer(soundPlayers+2, songs+2, Square, 4);
+	initSoundPlayer(soundPlayers+3, songs+3, Saw, 4);
+	
+	soundPlayers[0].state = Paused;
+	soundPlayers[1].state = Paused;
+	soundPlayers[2].state = Paused;
+	soundPlayers[3].state = Paused;
+	
+	time = 0;
+}
