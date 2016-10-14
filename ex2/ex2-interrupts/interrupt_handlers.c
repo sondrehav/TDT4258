2a5a9040f9e3486c39c773fd48ca8085b109a18e
Christian <christian_odden'hotmail.com>
2016-10-14 10:54:18 +0200

    buttons

--- ex2/ex2-interrupts/interrupt_handlers.c
+++ ex2/ex2-interrupts/interrupt_handlers.c
@@ -10,14 +10,28 @@
 
 	
 void playBeat(uint beat_index, uint time);
+void selectSong();
 
+uint currentSong = 0;
 
 uint songData[] = {
-	48, 50, 52, 53, 55, 55, 55, 55, 57, 57, 57, 57, 
-	55, 55, 55, 55, 53, 53, 53, 53, 52, 52, 52, 52, 
-	50, 50, 50, 50, 48, 48, 48, 48
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64
 };
-Song_t song = {songData, 32, 120, 1};
+Song_t song = {songData, 64, 120, 1};
+
+uint songData2[] = {
+	48 +5, 52 +5, 55+5, 60+5, 64+5, 60+5, 55+5, 52+5
+};
+Song_t song2 = {songData2, 8, 1920, 1};
+
+
 
 uint32_t beat_period_counter = 0;
 uint32_t count = 0;
@@ -30,15 +44,15 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 	   TODO feed new samples to the DAC
 	   remember to clear the pending interrupt by writing 1 to TIMER1_IFC
 	 */
-
-	uint beat_period = SAMPLE_RATE * 60 / song.tempo;
+	Song_t s = currentSong == 0 ? song : song2;
+	uint beat_period = SAMPLE_RATE * 60 / s.tempo;
 	playBeat(beat_index, count);
 			
 	beat_period_counter++;
-	if (beat_period_counter == beat_period) {
+	if (beat_period_counter >= beat_period) {
 		beat_period_counter = 0;
 		beat_index++;
-		if (beat_index == song.length) beat_index = 0;
+		if (beat_index >= s.length) beat_index = 0;
 	}
 	
 	count++;
@@ -50,24 +64,46 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler()
 {
 	/* TODO handle button pressed event, remember to clear pending interrupt */
-	*GPIO_PA_DOUT = 0xffffffff ^ *GPIO_PA_DOUT;
 	*GPIO_IFC = *GPIO_IF;
+	selectSong();
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 {
 	/* TODO handle button pressed event, remember to clear pending interrupt */
-	*GPIO_PA_DOUT = 0xffffffff ^ *GPIO_PA_DOUT;
 	*GPIO_IFC = *GPIO_IF;
+	selectSong();
+}
+
+void selectSong(){
+	if (((~*GPIO_PC_DIN) & 0x1) == 0x1){
+		currentSong = 0;
+		*GPIO_PA_DOUT = 0x0fff ^ *GPIO_PA_DOUT;
+	}
+	else if (((~*GPIO_PC_DIN) & 0x2) == 0x2)
+	{
+		*GPIO_PA_DOUT = 0xf0ff ^ *GPIO_PA_DOUT;
+		currentSong = 1;
+	}
+	else if (((~*GPIO_PC_DIN) & 0x4) == 0x4){
+		*GPIO_PA_DOUT = 0x5fff ^ *GPIO_PA_DOUT;
+		currentSong = 0;
+	}
+	else if (((~*GPIO_PC_DIN) & 0x8) == 0x8){
+		*GPIO_PA_DOUT = 0xf5ff ^ *GPIO_PA_DOUT;
+		currentSong = 1;
+	}
 }
 
 void playBeat(uint beat_index, uint time)
 {
-	fp frequency = getFreqNote(song.song[beat_index]);
+	Song_t s = currentSong == 0 ? song : song2;
+	fp frequency = getFreqNote(s.song[beat_index]);
 	fp sum = sawWave(frequency, time);
 	uint value = (sum*VOLUME) >> 16;
 	*DAC0_CH0DATA = value;
 	*DAC0_CH1DATA = value;	
 }
+
 
