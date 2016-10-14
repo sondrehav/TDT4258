d2f7d1574164c54ec6c25d87184179c111d8d6d9
Haakon Jonsson <haakonjonsson@gmail.com>
2016-10-14 11:24:09 +0200

    YAY!

--- ex2/ex2-interrupts/ex2.c
+++ ex2/ex2-interrupts/ex2.c
@@ -21,6 +21,47 @@ void setupTimer(uint32_t period);
 void setupDAC();
 void setupNVIC();
 
+
+
+/* Sound data and variables. */
+uint songData[] = {
+	48, 50, 52, 53, 55, 55, 55, 55, 57, 57, 57, 57, 
+	55, 55, 55, 55, 53, 53, 53, 53, 52, 52, 52, 52, 
+	50, 50, 50, 50, 48, 48, 48, 48
+};
+
+uint songData2[] = {
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	48, 50, 55, 62, 65, 55, 62, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	47, 50, 55, 61, 65, 55, 61, 65, 
+	48, 52, 55, 60, 64, 55, 60, 64,
+	48, 52, 55, 60, 64, 55, 60, 64
+};
+
+uint songData3[] = {
+	0, 0, 50
+};
+
+uint songData4[] = {
+	100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0
+};
+
+song_t songs[4];
+soundPlayer_t soundPlayers[4];
+audio_t audio = {soundPlayers, 4};
+
+song_t song1 = {songData, 32, 240, 0};
+song_t song2 = {songData2, 64, 480, 0};
+song_t song3 = {songData3, 3, 60, 0};
+song_t song4 = {songData4, 11, 120, 0};
+
+uint time;
+
+
+
 /* Your code will start executing here */
 int main(void)
 {
@@ -32,6 +73,24 @@ int main(void)
 	/* Enable interrupt handling */
 	setupNVIC();
 
+	/* Audio setup. */
+	songs[0] = song1;
+	songs[1] = song2;
+	songs[2] = song3;
+	songs[3] = song4;
+	
+	initSoundPlayer(soundPlayers, songs, Saw, 4);
+	initSoundPlayer(soundPlayers+1, songs+1, Saw, 4);
+	initSoundPlayer(soundPlayers+2, songs+2, Saw, 4);
+	initSoundPlayer(soundPlayers+3, songs+3, Saw, 4);
+	
+	soundPlayers[0].state = Paused;
+	soundPlayers[1].state = Paused;
+	soundPlayers[2].state = Paused;
+	soundPlayers[3].state = Paused;
+	
+	time = 0;
+	
 	/* TODO for higher energy efficiency, sleep while waiting for interrupts
 	   instead of infinite loop for busy-waiting
 	 */
--- ex2/ex2-interrupts/interrupt_handlers.c
+++ ex2/ex2-interrupts/interrupt_handlers.c
@@ -7,37 +7,12 @@
 #include "sound_generator.h"
 #include "sound_player.h"
 
-/* TIMER1 interrupt handler */
-
-	
-void playBeat(uint beat_index, uint time);
 void selectSong();
 
-uint currentSong = 0;
+extern audio_t audio;
+extern uint time;
 
-uint songData[] = {
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 50, 55, 62, 65, 55, 62, 65, 
-	48, 50, 55, 62, 65, 55, 62, 65, 
-	47, 50, 55, 61, 65, 55, 61, 65, 
-	47, 50, 55, 61, 65, 55, 61, 65, 
-	48, 52, 55, 60, 64, 55, 60, 64,
-	48, 52, 55, 60, 64, 55, 60, 64
-};
-song_t song = {songData, 64, 120, 1};
-
-uint songData2[] = {
-	48 +5, 52 +5, 55+5, 60+5, 64+5, 60+5, 55+5, 52+5
-};
-song_t song2 = {songData2, 8, 1920, 1};
-
-
-
-uint32_t beat_period_counter = 0;
-uint32_t count = 0;
-uint32_t beat_index = 0;
-
+/* TIMER1 interrupt handler */
 
 void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 {
@@ -45,18 +20,8 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 	   TODO feed new samples to the DAC
 	   remember to clear the pending interrupt by writing 1 to TIMER1_IFC
 	 */
-	song_t s = currentSong == 0 ? song : song2;
-	uint beat_period = SAMPLE_RATE * 60 / s.tempo;
-	playBeat(beat_index, count);
-			
-	beat_period_counter++;
-	if (beat_period_counter >= beat_period) {
-		beat_period_counter = 0;
-		beat_index++;
-		if (beat_index >= s.length) beat_index = 0;
-	}
-	
-	count++;
+	playAudio(&audio, time);
+	time++;
 
 	*TIMER1_IFC = 0x1;
 }
@@ -78,31 +43,24 @@ void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 }
 
 void selectSong(){
-	if (((~*GPIO_PC_DIN) & 0x1) == 0x1){
-		currentSong = 0;
+	if (((~*GPIO_PC_DIN) & 0x1) == 0x1)
+	{
+		audio.sounds[0].state = Running;
 		*GPIO_PA_DOUT = 0x0fff ^ *GPIO_PA_DOUT;
 	}
 	else if (((~*GPIO_PC_DIN) & 0x2) == 0x2)
 	{
+		audio.sounds[1].state = Running;
 		*GPIO_PA_DOUT = 0xf0ff ^ *GPIO_PA_DOUT;
-		currentSong = 1;
 	}
-	else if (((~*GPIO_PC_DIN) & 0x4) == 0x4){
+	else if (((~*GPIO_PC_DIN) & 0x4) == 0x4)
+	{
+		audio.sounds[2].state = Running;
 		*GPIO_PA_DOUT = 0x5fff ^ *GPIO_PA_DOUT;
-		currentSong = 0;
 	}
-	else if (((~*GPIO_PC_DIN) & 0x8) == 0x8){
+	else if (((~*GPIO_PC_DIN) & 0x8) == 0x8)
+	{
+		audio.sounds[3].state = Running;
 		*GPIO_PA_DOUT = 0xf5ff ^ *GPIO_PA_DOUT;
-		currentSong = 1;
 	}
-}
-
-void playBeat(uint beat_index, uint time)
-{
-	song_t s = currentSong == 0 ? song : song2;
-	fp frequency = getFreqNote(s.song[beat_index]);
-	fp sum = sawWave(frequency, time);
-	uint value = (sum*VOLUME) >> 16;
-	*DAC0_CH0DATA = value;
-	*DAC0_CH1DATA = value;	
 }
