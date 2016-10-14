78a94c003f381fd99fef183f8277209c5823327b
Haakon Jonsson <haakonjonsson@gmail.com>
2016-10-14 13:31:51 +0200

    comments

--- ex2/ex2-interrupts/sound_player.c
+++ ex2/ex2-interrupts/sound_player.c
@@ -7,14 +7,16 @@ void initSoundPlayer(soundPlayer_t* player, song_t* song, soundType_t soundType,
 	player->state = Running;
 	player->noteIndex = 0;
 	player->noteCounter = 0;
-	player->notePeriod = SAMPLE_RATE * 60 / song->tempo;
+	player->notePeriod = SAMPLE_RATE * 60 / song->tempo; // Calculate note period in sample rate ticks.
 	player->soundType = soundType;
 	player->volume = volume;
 }
 
 fp playSong(soundPlayer_t* player, uint time) {
-	fp freq = getFreqNote(player->song->song[player->noteIndex]);
+	fp freq = getFreqNote(player->song->song[player->noteIndex]); // Get frequency of current note.
 	fp sample;
+	
+	// Sample using specified waveform.
 	switch(player->soundType) {
 		case Saw:
 			sample = sawWave(freq, time);
@@ -28,28 +30,32 @@ fp playSong(soundPlayer_t* player, uint time) {
 	}
 	
 	player->noteCounter++;
-	if (player->noteCounter == player->notePeriod) {
+	if (player->noteCounter == player->notePeriod) { // End of note.
 		player->noteIndex++;
 		player->noteCounter = 0;
-		if (player->noteIndex == player->song->length) {
+		if (player->noteIndex == player->song->length) { // End of song.
 			player->noteIndex = 0;
 			if (!player->song->looping) {
 				player->state = Done;
 			}
 		}
 	}
-	sample *= player->volume;
+	sample *= player->volume; // Scale with specified volume
 	return sample;
 }
 
 void playAudio(audio_t* audio, uint time) {
 	fp sum = 0;
+	
+	// Sample all sound tracks currently playing.
 	for (uint i = 0; i<audio->soundCount; i++) {
 		if (audio->sounds[i].state == Running) {
 			sum += playSong(&audio->sounds[i], time);
 		}
 	}
-	sum >>= 16;
+	sum >>= 16; // Convert from fixed point to unsigned int.
+	
+	// Set the DAC sample registers.
 	*DAC0_CH0DATA = sum;
 	*DAC0_CH1DATA = sum;
 }
