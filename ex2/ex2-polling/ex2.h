4a254208571673e88b7f87ced27c718effe3f531
Haakon Jonsson <haakonjonsson@gmail.com>
2016-10-17 18:50:21 +0200

    Removed volume define

--- ex2/ex2-polling/ex2.h
+++ ex2/ex2-polling/ex2.h
@@ -4,10 +4,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* The period between sound samples, in clock cycles */
+/* The frequency at which new samples are given to the DAC. */
 #define SAMPLE_RATE		12000
-#define	SAMPLE_PERIOD	14000000 / SAMPLE_RATE // 14 MHz / 292 = 48 KHz
-#define VOLUME 			128
+#define	SAMPLE_PERIOD	14000000 / SAMPLE_RATE
 
 typedef uint32_t uint;
 typedef uint32_t fp;
