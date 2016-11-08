4559de178ed6853db89a90306b5fc40f9f34c18f
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2016-11-08 12:58:33 +0100

    ...

--- ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void input_handler(uint32_t value);
+
 
 int main(int argc, char *argv[])
 {
+	
 	printf("Hello World, I'm game!\n");
 	FILE* file = fopen("dev/GamepadDriver", "r+w+");
 	char buf[256];
-
+	
+	signal(SIGIO, &input_handler); /* dummy sample; sigaction() is better */ 
+	fcntl(STDIN_FILENO, F_SETOWN, getpid());
+	int oflags = fcntl(STDIN_FILENO, F_GETFL);
+	fcntl(STDIN_FILENO, F_SETFL, oflags | FASYNC);
+	
 	fprintf(file, "Test for driver!");
 	
 	int i = 0;
@@ -19,8 +27,12 @@ int main(int argc, char *argv[])
 		i++; 
 		sleep(1);
 	}
-	
 
 	fclose(file);
 	exit(EXIT_SUCCESS);
+}
+
+void input_handler(uint32_t value){
+	printf("%x\n", value);
+	
 }
