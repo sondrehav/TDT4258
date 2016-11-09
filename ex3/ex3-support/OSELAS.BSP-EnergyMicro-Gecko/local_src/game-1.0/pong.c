02cfd60cf79aebd7397c2af15938bd05363e1879
Sondre Havellen <sondre.havellen@gmail.com>
2016-11-09 12:53:49 +0100

    Test of frame buffer

---
+++ ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.c
@@ -0,0 +1,34 @@
+#include "pong.h"
+
+bool running = true;
+FILE* framebuffer;
+
+
+typedef uint16_t color;
+
+
+color createColor(uint8_t r, uint8_t g, uint8_t b)
+{
+	color c = 0;
+	c |= (((uint16_t)r >> 3) << 11);
+	c |= (((uint16_t)g >> 2) << 5);
+	c |= (((uint16_t)b >> 3) << 0);
+	return c;
+}
+
+static void enterGame(FILE* framebufferDriver) {
+	framebuffer = framebufferDriver;
+	static uint16_t* arr = {7, 643, 1, 35635, 1244, 4574, 58753, 34325};
+	fwrite(arr, sizeof(uint16_t), 8, framebuffer);
+	sleep(30);
+}
+
+static void onKeyDown(uint32_t key) {
+	static uint16_t* arr = {5 * key, 11 * key, 13 * key, 17 * key, 19 * key, 23 * key, 29 * key, 31 * key};
+	fwrite(arr, sizeof(uint16_t), 8, framebuffer);
+}
+
+static void onKeyUp(uint32_t key) {
+	static uint16_t* arr = {0, 0, 0, 0, 0, 0, 0, 0};
+	fwrite(arr, sizeof(uint16_t), 8, framebuffer);	
+}
---
+++ ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.h
@@ -0,0 +1,8 @@
+
+
+static void enterGame(FILE* framebufferDriver);
+
+static void onKeyDown(uint32_t key);
+
+static void onKeyUp(uint32_t key);
+
