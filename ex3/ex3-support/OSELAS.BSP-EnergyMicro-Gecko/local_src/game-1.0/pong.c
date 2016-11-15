4fbb20515022b48f5d4b15e502c84f3df31d4966
Sondre Havellen <sondre.havellen@gmail.com>
2016-11-15 10:46:00 +0100

    Draw board method

--- ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.c
+++ ex3/ex3-support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/pong.c
@@ -5,6 +5,14 @@ FILE* framebuffer;
 
 typedef uint16_t color;
 
+#define PIXEL_DIM 		5
+#define V_SCREEN_WIDTH 	64
+#define V_SCREEN_HEIGHT 48
+#define SCREEN_WIDTH 	320
+#define SCREEN_HEIGHT 	240
+
+void drawBoard();
+
 static color createColor(uint8_t r, uint8_t g, uint8_t b)
 {
 	color c = 0;
@@ -32,6 +40,7 @@ static void drawRectangle(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, co
 void enterGame(FILE* framebufferDriver) {
 	framebuffer = framebufferDriver;
 	drawRectangle(0,0,320,240, createColor(0,0,0));
+	drawBoard();
 	for (int j=0; j<30; j++){
 		sleep(1);
 	}
@@ -43,4 +52,56 @@ void onKeyDown(uint32_t key) {
 
 void onKeyUp(uint32_t key) {
 	drawRectangle(key * 20, key * 20, (key + 1) * 20, (key + 1) * 20, createColor(0, 0, 0));
+}
+
+void toScreenSpace(uint32_t* x0, uint32_t* y0, uint32_t* x1, uint32_t* y1){
+	*(x0) = *(x0) * SCREEN_WIDTH / V_SCREEN_WIDTH;
+	*(x1) = *(x1) * SCREEN_WIDTH / V_SCREEN_WIDTH;
+	*(y0) = *(y0) * SCREEN_HEIGHT / V_SCREEN_HEIGHT;
+	*(y1) = *(y1) * SCREEN_HEIGHT / V_SCREEN_HEIGHT;
+}
+
+void drawBoard(){
+	color = createColor(255, 255, 255);
+	{ // mid line
+		uint32_t mid = V_SCREEN_WIDTH / 2;
+		uint32_t x0 = mid;
+		uint32_t x1 = mid + 1;
+		uint32_t y0 = 0;
+		uint32_t y1 = V_SCREEN_HEIGHT;
+		toScreenSpace(&x0, &y0, &x1, &y1);
+		drawRectangle(x0, y0, x1, y1, color);
+	}
+	{ // top line
+		uint32_t x0 = 0;
+		uint32_t x1 = V_SCREEN_WIDTH;
+		uint32_t y0 = 0;
+		uint32_t y1 = 1;
+		toScreenSpace(&x0, &y0, &x1, &y1);
+		drawRectangle(x0, y0, x1, y1, color);
+	}
+	{ // bottom line
+		uint32_t x0 = 0;
+		uint32_t x1 = V_SCREEN_WIDTH;
+		uint32_t y0 = V_SCREEN_HEIGHT - 1;
+		uint32_t y1 = V_SCREEN_HEIGHT;
+		toScreenSpace(&x0, &y0, &x1, &y1);
+		drawRectangle(x0, y0, x1, y1, color);
+	}
+	{ // left line
+		uint32_t x0 = 0;
+		uint32_t x1 = 1;
+		uint32_t y0 = 0;
+		uint32_t y1 = V_SCREEN_HEIGHT;
+		toScreenSpace(&x0, &y0, &x1, &y1);
+		drawRectangle(x0, y0, x1, y1, color);
+	}
+	{ // right line
+		uint32_t x0 = V_SCREEN_WIDTH - 1;
+		uint32_t x1 = V_SCREEN_WIDTH;
+		uint32_t y0 = 0;
+		uint32_t y1 = V_SCREEN_HEIGHT;
+		toScreenSpace(&x0, &y0, &x1, &y1);
+		drawRectangle(x0, y0, x1, y1, color);
+	}
 }
